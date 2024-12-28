f909730b4f790e9c040b820b6fc19bf22addfea6
daviidx23 <davidmas.sm@gmail.com>
2024-12-28 22:38:33 +0100

    Problem 2620 solved

--- 2620_Counter.js
+++ 2620_Counter.js
@@ -3,15 +3,18 @@
  * @return {Function} counter
  */
 var createCounter = function(n) {
-    
-    return function() {
+    return function(i) {
+        let counter = n;
+        n++;
         
+        return counter;
+
     };
 };
 
-/** 
- * const counter = createCounter(10)
- * counter() // 10
- * counter() // 11
- * counter() // 12
- */
+const counter = createCounter(10)
+console.log(counter())
+console.log(counter())
+console.log(counter())
+
+ 
