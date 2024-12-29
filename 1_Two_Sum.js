543769266f5a91001d741cc6ca5ac3bf200939e4
daviidx23 <davidmas.sm@gmail.com>
2024-12-29 14:27:15 +0100

    Problem 1 Solved

--- 1_Two_Sum.js
+++ 1_Two_Sum.js
@@ -4,5 +4,15 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    
+    for(i=0;i<nums.length;i++){
+        for(j=i+1;j<nums.length;j++){
+            if(nums[i]+nums[j]===target){
+                return [i, j]
+            }
+        }
+    }
 };
+
+nums = [3,3], target = 6
+let res = twoSum(nums,target)
+console.log(res)
