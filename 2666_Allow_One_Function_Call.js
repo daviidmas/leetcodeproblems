6c182b3dc9f63c52386424ed749009e2bec45dc0
daviidx23 <davidmas.sm@gmail.com>
2024-12-29 14:13:41 +0100

    Problem 2666 Solved

--- 2666_Allow_One_Function_Call.js
+++ 2666_Allow_One_Function_Call.js
@@ -3,16 +3,20 @@
  * @return {Function}
  */
 var once = function(fn) {
-    
+    let first = true
     return function(...args){
-        
+        if (first) {
+            first = false
+            return fn(...args)
+        } else {
+            return undefined
+        }
     }
 };
 
-/**
- * let fn = (a,b,c) => (a + b + c)
- * let onceFn = once(fn)
- *
- * onceFn(1,2,3); // 6
- * onceFn(2,3,6); // returns undefined without calling fn
- */
+let fn = (a,b,c) => (a + b + c)
+let onceFn = once(fn)
+
+console.log(onceFn(1,2,3)); // 6
+console.log(onceFn(2,3,6)); // returns undefined without calling fn
+ 
