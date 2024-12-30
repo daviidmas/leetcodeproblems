70451e8529dab38b195228f2bacf6882d540a2d4
daviidx23 <davidmas.sm@gmail.com>
2024-12-30 23:14:05 +0100

    Problem 27 Solved

--- 27_Remove_Element.js
+++ 27_Remove_Element.js
@@ -4,8 +4,19 @@
  * @return {number}
  */
 var removeElement = function(nums, val) {
-    
+    let nlength = nums.length
+    let k = 0
+    for(let i=0;i<nlength;i++){
+        if(nums[i]==val) {
+            for(let j=i;j<nums.length;j++){
+                nums[j] = nums[j+1]
+            }
+            k++
+            i--
+        }
+    }
+    return nlength-k
 };
 
-nums = [3,2,2,3], val = 3
+nums = [0,1,2,2,3,0,4,2], val = 2
 console.log(removeElement(nums, val))
