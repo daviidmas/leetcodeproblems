9cb03cad945ede3f82e15e641e2c31062ea325e4
daviidx23 <davidmas.sm@gmail.com>
2024-12-30 22:36:18 +0100

    Problem 27 Added

---
+++ 27_Remove_Element.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function(nums, val) {
+    
+};
+
+nums = [3,2,2,3], val = 3
+console.log(removeElement(nums, val))
