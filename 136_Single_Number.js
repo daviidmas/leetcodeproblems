b4810012796f2a92edb5d48df3b2555b25e7aa03
daviidx23 <davidmas.sm@gmail.com>
2025-01-01 22:40:03 +0100

    Problem 136 Solved

--- 136_Single_Number.js
+++ 136_Single_Number.js
@@ -3,5 +3,25 @@
  * @return {number}
  */
 var singleNumber = function(nums) {
-    
+    let res
+    for(let i=0;i<nums.length;i++){
+        let repeated = false
+        for(let j=i+1;j<nums.length;j++) {
+            if(nums[i]==nums[j]) {
+                repeated = true
+            }
+        }
+        for(let j=i-1;j>=0;j--) {
+            if(nums[i]==nums[j]) {
+                repeated = true
+            }
+        } 
+        if(!repeated) {
+            res = nums[i]
+        } 
+    }
+    return res
 };
+
+nums = [1]
+console.log(singleNumber(nums))
