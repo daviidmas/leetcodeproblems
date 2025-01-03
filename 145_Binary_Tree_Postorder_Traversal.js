ae1d1834cfb1a4083cbd568053c5f43f487ad255
daviidx23 <davidmas.sm@gmail.com>
2025-01-03 22:29:27 +0100

    Problem 145 Solved

--- 145_Binary_Tree_Postorder_Traversal.js
+++ 145_Binary_Tree_Postorder_Traversal.js
@@ -6,13 +6,36 @@
  *     this.right = (right===undefined ? null : right)
  * }
  */
+
 /**
  * @param {TreeNode} root
  * @return {number[]}
  */
 var postorderTraversal = function(root) {
+    const result = [];
     
+    function traverse(node) {
+        if (node === null) {
+            return;
+        }
+        
+        // First traverse left subtree
+        traverse(node.left);
+        
+        // Then traverse right subtree
+        traverse(node.right);
+        
+        // Finally add the root value
+        result.push(node.val);
+    }
+    
+    traverse(root);
+    return result;
 };
 
-root = [1,null,2,3]
-console.log(postorderTraversal(root))
+// Constructing the tree [1,null,2,3]
+let node3 = new TreeNode(3);
+let node2 = new TreeNode(2, node3, null);
+let root = new TreeNode(1, null, node2);
+
+console.log(postorderTraversal(root)); // [3,2,1]
